c013528a54494b4236f73621a2beb06e465c20b8
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-27 16:01:54 -0500

    add template to flake outputs

--- flake.nix
+++ flake.nix
@@ -5,33 +5,41 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-22.05";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    supportedSystems = [
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
-    genSystems = nixpkgs.lib.genAttrs supportedSystems;
-    pkgs = genSystems (system: import nixpkgs {inherit system;});
-  in {
-    devShell = genSystems (system: let
-      inherit (pkgs.${system}) mkShell reveal-md;
+  outputs =
+    { self
+    , nixpkgs
+    ,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      genSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = genSystems (system: import nixpkgs { inherit system; });
     in
-      mkShell {
-        packages = [
-          (reveal-md.override {
-            dontNpmInstall = true;
-            prePatch = ''
-              export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1
-            '';
-            postInstall = ''
-              mkdir -p $out/bin
-              ln -sf $out/lib/node_modules/reveal-md/bin/reveal-md.js $out/bin/reveal-md
-            '';
-          })
-        ];
-      });
-  };
+    {
+      templates.default = {
+        path = ./template;
+        description = "A simple flake with slightly better defaults than normal.";
+      };
+      devShell = genSystems (system:
+        let
+          inherit (pkgs.${system}) mkShell reveal-md;
+        in
+        mkShell {
+          packages = [
+            (reveal-md.override {
+              dontNpmInstall = true;
+              prePatch = ''
+                export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1
+              '';
+              postInstall = ''
+                mkdir -p $out/bin
+                ln -sf $out/lib/node_modules/reveal-md/bin/reveal-md.js $out/bin/reveal-md
+              '';
+            })
+          ];
+        });
+    };
 }
